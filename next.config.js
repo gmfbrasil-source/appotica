3d44d93d6a42840bcd6406e6e379e0130a95791f
gmfbrasil-source <gmfbrasil@gmail.com>
2026-06-17 11:23:30 -0300

    add

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow pages to be exported with TypeScript errors.
+    // This will allow the build to complete even if there are type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
